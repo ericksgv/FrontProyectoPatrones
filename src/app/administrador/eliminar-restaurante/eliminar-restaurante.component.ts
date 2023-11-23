ce8ed639faacfe384a78d17658372f5a8b4b9091
Kevin López <kevinleonardolopez@gmail.com>
2023-11-22 23:46:28 -0500

    Eliminar restaurante

--- src/app/administrador/eliminar-restaurante/eliminar-restaurante.component.ts
+++ src/app/administrador/eliminar-restaurante/eliminar-restaurante.component.ts
@@ -1,4 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AdminServiceService } from '../service/admin-service.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-eliminar-restaurante',
@@ -7,4 +11,60 @@ import { Component } from '@angular/core';
 })
 export class EliminarRestauranteComponent {
 
+  plazoletaId: number = 0;
+  plazoletaName: string = "";
+
+  restauranteId : number = 0 ; 
+
+  constructor(private http: HttpClient, private router: Router, private adminService: AdminServiceService) {}
+
+  ngOnInit(): void {
+    this.plazoletaId = +localStorage.getItem("plazoletaIdAdmin")!;
+  
+    if (this.plazoletaId) {
+      // Fetch plazoleta name
+      this.adminService.obtenerNombrePlazoletaPorId(this.plazoletaId)
+        .subscribe(
+          (plazoleta: PlazoletaDTO) => {
+            this.plazoletaName = plazoleta.nombre;
+          },
+          (error) => {
+            console.error('Error fetching plazoleta name:', error);
+          }
+        );
+    }
+  }
+
+  onSubmit(): void {
+    // Llamada al servicio para eliminar el restaurante
+    this.adminService.eliminarRestaurante(this.restauranteId)
+      .subscribe(
+        (response) => {
+          console.log('Restaurante eliminado exitosamente:', response);
+          Swal.fire({
+            icon: 'success',
+            title: 'OK',
+            text: 'Restaurante eliminado exitosamente',
+          });
+          this.router.navigate(['/menu-admin']);
+          // Puedes redirigir a otra página o realizar otras acciones después de la eliminación
+        },
+        (error) => {
+          console.error('Error al eliminar el restaurante:', error);
+          Swal.fire({
+            icon: 'error',
+            title: 'error',
+            text: 'El restaurante contiene pedidos o no existe',
+          });
+        }
+      );
+  }
+
 }
+
+
+interface PlazoletaDTO {
+  id: number;
+  nombre: string;
+}
+
